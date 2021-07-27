285755fed7afe8084326a4557b0b50d36968da40
Calvin Storoschuk <calvinsomething@gmail.com>
2021-07-27 10:43:38 -0400

    add logging with winston

---
+++ startup/logging.js
@@ -0,0 +1,30 @@
+const winston = require('winston');
+require('express-async-errors');
+require('winston-mongodb');
+
+module.exports = () => {
+    let consoleLevel = 'error';
+    if (process.env.NODE_ENV != 'production')
+        consoleLevel = 'info';
+
+    winston.configure({
+        transports: [
+            new winston.transports.File({
+                filename: 'portfo.log',
+                level: 'error',
+                format: winston.format.simple(),
+                handleExceptions: true,
+                handleRejections: true
+            }),
+            new winston.transports.Console({
+                level: consoleLevel,
+                format: winston.format.combine(
+                    winston.format.colorize(),
+                    winston.format.simple(),
+                ),
+                handleExceptions: true,
+                handleRejections: true
+            })
+        ]
+    });
+};
