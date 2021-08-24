795bbf76ce9800b125982ac3be969cd070220cab
Ubuntu <calvin@portfo.q1wdo0gyth1exmn1nv42wsqhtc.bx.internal.cloudapp.net>
2021-08-24 22:11:04 +0000

    add https

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const http = require('http');
+const fs = require('fs');
+const https = require('https');
 const winston = require('winston');
 
 const app = require('./app');
@@ -6,6 +7,15 @@ const app = require('./app');
 const PORT = process.env.PORT;
 if (!PORT) throw new Error('Must define "PORT" env variable.');
 
-const server = http.createServer(app);
+const key = fs.readFileSync('/etc/letsencrypt/live/calvinsomething.com/privkey.pem');
+const cert = fs.readFileSync('/etc/letsencrypt/live/calvinsomething.com/fullchain.pem');
+
+winston.info(key);
+winston.info(cert);
+
+const server = https.createServer({
+    key: key,
+    cert: cert
+}, app);
 
 server.listen(PORT, () => winston.info(`Listening on port ${PORT}...`));
